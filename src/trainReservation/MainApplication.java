f1b709b31a0839ffe0bacf5f8330191f47627f16
jhe2426 <jhe2426@naver.com>
2023-03-13 10:16:14 +0900

    first commit

---
+++ src/trainReservation/MainApplication.java
@@ -0,0 +1,9 @@
+package trainReservation;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Run Test");
+	}
+
+}
---
+++ src/trainReservation/entity/Train.java
@@ -0,0 +1,5 @@
+package trainReservation.entity;
+
+public class Train {
+
+}
